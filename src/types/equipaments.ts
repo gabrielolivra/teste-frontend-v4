94659c1190f35fc6d2e82a3811929aec6a77de77
gabrielolivra <gabernardinooliveira@gmail.com>
2025-03-24 18:16:45 -0300

    feat: types

---
+++ src/types/equipaments.ts
@@ -0,0 +1,38 @@
+export interface IEquipaments {
+  id: string,
+  name: string,
+  equipmentModelId: string
+}
+
+
+export interface IEquipamentsModel {
+  id: string,
+  name: string,
+  hourlyEarnings: {
+    equipmentStateId: string,
+    value: string
+  }[]
+}
+
+export interface IEquipamentsPositionHistory {
+  equipmentId: string,
+  positions: {
+    date: string,
+    lat: number,
+    lon: number
+  }[]
+}
+
+export interface IEquipamentsState {
+  id:string,
+  name:string,
+  color:string
+}
+
+export interface IEquipamentsStateHistory {
+  equipmentId:string,
+  states: {
+    date:string,
+    equipmentStateId:string
+  }[]
+}
